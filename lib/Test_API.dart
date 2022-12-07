350c58d0c6dc04e8993bdbd1bb272a320abf497f
narongrit.up.63 <sirtongzaq@gmail.com>
2022-12-07 11:43:35 +0700

    Update 12/7/2022

--- lib/Test_API.dart
+++ lib/Test_API.dart
@@ -9,23 +9,22 @@ class TestAPI extends StatefulWidget {
   @override
   _TestAPIState createState() => _TestAPIState();
 }
-class _TestAPIState extends State<TestAPI> {
 
+class _TestAPIState extends State<TestAPI> {
   List<Team> teams = [];
 
   // get team
   Future getTeams() async {
-    var response = await http.get(Uri.http("084c-2001-fb1-14b-4d3-9d25-b3a3-f422-cf1c.ap.ngrok.io","test"));
+    var response = await http.get(Uri.http("192.168.1.50:5000", "test"));
     var jsonData = jsonDecode(response.body);
     var x = jsonData['data'];
     print(x);
 
-
     for (var eachTeam in jsonData['data']) {
       final team = Team(
-        Store: eachTeam['Store'], 
+        Store: eachTeam['Store'],
         score: eachTeam['score'],
-        );
+      );
       teams.add(team);
     }
   }
@@ -34,41 +33,40 @@ class _TestAPIState extends State<TestAPI> {
   Widget build(BuildContext context) {
     getTeams();
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Test API'),
-        ),
-        body: FutureBuilder(
-          future: getTeams(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return ListView.builder(
-                itemCount: teams.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Column(
-                        children: [
-                          Text(teams[index].Store),
-                          Text(teams[index].score.toString()),
-                        ],
-                      ),
+      appBar: AppBar(
+        title: const Text('Test API'),
+      ),
+      body: FutureBuilder(
+        future: getTeams(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
                     ),
-                  );
-                },
-              );
-            }
-            else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
-        ),
-      );
+                    child: Column(
+                      children: [
+                        Text(teams[index].Store),
+                        Text(teams[index].score.toString()),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
   }
 }
